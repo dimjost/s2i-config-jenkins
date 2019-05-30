6fd45dd89d667736eb864991a079c7d52ebcda4b
Dimitri Jost <dimitri.jost@gmx.eu>
2019-05-30 22:07:14 +0200

    Add Maven configuration/setup

---
+++ configuration/init.groovy.d/configure-maven.groovy
@@ -0,0 +1,64 @@
+import hudson.model.*;
+import jenkins.model.*;
+import hudson.tools.*;
+import hudson.tasks.Maven.MavenInstaller;
+import hudson.tasks.Maven.MavenInstallation;
+
+def env = System.getenv()
+
+// Variables
+// MAVEN_VERSION can be defined as a single version or a comma separated string of versions
+// eg.
+// MAVEN_VERSION=3.0.5
+// MAVEN_VERSION=3.0.5,3.2.5
+
+def maven_version = "3.6.1,3.6.0"
+def maven_version_list = maven_version.split(',')
+
+// Constants
+def instance = Jenkins.getInstance()
+
+
+	// Maven
+	println "--> Configuring Maven"
+	def desc_MavenTool = instance.getDescriptor("hudson.tasks.Maven")
+	def maven_installations = desc_MavenTool.getInstallations()
+
+	maven_version_list.eachWithIndex { version, index ->
+		def mavenInstaller = new MavenInstaller(version)
+		def installSourceProperty = new InstallSourceProperty([mavenInstaller])
+		
+		def name="maven_" + version
+
+		// This makes the solution backwards-compatible, and will treat the first version in the array as "Maven"
+		if (index == 0) {
+			name="Maven"
+		}
+
+		def maven_inst = new MavenInstallation(
+		  name, // Name
+		  "", // Home
+		  [installSourceProperty]
+		)
+
+		// Only add a Maven installation if it does not already exist - do not overwrite existing config
+		
+		def maven_inst_exists = false
+		maven_installations.each {
+		  installation = (MavenInstallation) it
+			if ( maven_inst.getName() ==  installation.getName() ) {
+					maven_inst_exists = true
+					println("Found existing installation: " + installation.getName())
+			}
+		}
+		
+		if (!maven_inst_exists) {
+			maven_installations += maven_inst
+		}
+	}
+
+	desc_MavenTool.setInstallations((MavenInstallation[]) maven_installations)
+	desc_MavenTool.save()
+
+	// Save the state
+	instance.save()
